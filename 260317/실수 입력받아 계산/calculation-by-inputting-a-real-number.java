4dd5c044639f414bffa7482bbc55ebff9e3db395
wonjunx <70313647+wonjunx@users.noreply.github.com>
2026-03-17 22:11:26 +0900

    Title: 실수 입력받아 계산, Time: 147ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 260317/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double sum = a + b;
+        System.out.printf("%.2f", sum);
+    }
+}
