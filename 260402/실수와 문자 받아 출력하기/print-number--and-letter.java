fac40264956a1b29a4a7b672221e19fa884e9566
wonjunx <70313647+wonjunx@users.noreply.github.com>
2026-04-02 17:50:52 +0900

    Title: 실수와 문자 받아 출력하기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260402/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        Sysem.out.println(c);
+        Sysem.out.println(c);
+    }
+}
