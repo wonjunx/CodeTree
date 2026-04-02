74124bde046f76b5c008046db253bd88cd80d65f
wonjunx <70313647+wonjunx@users.noreply.github.com>
2026-04-02 17:54:04 +0900

    Title: 실수와 문자 받아 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/실수와 문자 받아 출력하기/print-number--and-letter.java
+++ 260402/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char c = sc.next();
+        String c = sc.next();
         double a = sc.nextDouble();
         double b = sc.nextDouble();
-        Sysem.out.println(c);
-        Sysem.out.println(c);
+        System.out.println(c);
+        System.out.printf("%.2f\n", a);
+        System.out.printf("%.2f\n", b);
     }
 }
